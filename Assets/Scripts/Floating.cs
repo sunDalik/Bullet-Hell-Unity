dbe702883962f2eea805d74616be60dbe5b2e4bb
sunDalik <dali1999@mail.ru>
2020-11-07 01:22:35 +0300

    Add chicken mystery

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    private bool inWater = false;
+    private Collider currentWater;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (inWater)
+        {
+
+            Vector3 pos = transform.position;
+            float waterY = currentWater.transform.position.y;
+            pos.y = waterY;
+            //transform.position = pos;
+
+            if (transform.position.y < currentWater.transform.position.y)
+            {
+                rb.AddForce(new Vector3(0, 25f * rb.mass, 0), ForceMode.Force);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            inWater = true;
+            currentWater = other;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 4)
+        {
+            inWater = false;
+        }
+    }
+}
